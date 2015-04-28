9c3526d6fe301223b22ea3884ef1ff193f6ba31b
Dakoda Wogan <d.dakoda@yahoo.com>
2015-04-28 14:30:53 -0500

    establishing admin

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,39 +1,69 @@
-console.log('coffee, on coffee, on coffee');
+var app = app || {};
 
-var foodModel = Backbone.Model.extend();
-
-var foodTemplate = $('#food-template').html();
-
-var foodCollection = Backbone.Collection.extend();
-
-var foodView = Backbone.View.extend({
-  tagName: "p",
-  className: "food",
-  template: _.template(foodTemplate),
-  initialize: function() {
-    console.log("view == initialized");
-    this.listenTo(this.model, "change", this.render);
-  },
-  render: function() {
-    console.log("view == rendered");
-    var data = this.model.attributes;
-    var tpl = this.template(data);
-    this.$el.html(tpl);
-    $("#food-container").append(this.$el.html());
-  }
-});
+$(document).ready(function(){
 
 
-$(document).ready(function(evt) {
+  app.foods = new app.FoodCollection({
+    model: app.FoodModel
+  })
 
-  var foodModel = new foodModel();
-  foodModel.url = '/api/food';
+  app.parties = new app.PartyCollection({
+    model: app.PartyModel
+  })
 
-  var foodCollection = new foodCollection();
-  foodCollection.url = '/api/food';
+  app.foodListPainter = new app.GeneralListView({
+    modelView: app.FoodView,
+    collection: app.foods,
+    el: $('#menu-list'),
   });
 
-  foodCollection.fetch();
+  app.partyListPainter = new app.GeneralListView({
+    modelView: app.PartyView,
+    collection: app.parties,
+    el: $('#party-list'),
+  });
+
+  app.parties.fetch();
+  app.foods.fetch();
+
+});
+
+// console.log('coffee, on coffee, on coffee');
+//
+// var foodModel = Backbone.Model.extend();
+//
+// var foodTemplate = $('#food-template').html();
+//
+// var foodCollection = Backbone.Collection.extend();
+//
+// var foodView = Backbone.View.extend({
+//   tagName: "p",
+//   className: "food",
+//   template: _.template(foodTemplate),
+//   initialize: function() {
+//     console.log("view == initialized");
+//     this.listenTo(this.model, "change", this.render);
+//   },
+//   render: function() {
+//     console.log("view == rendered");
+//     var data = this.model.attributes;
+//     var tpl = this.template(data);
+//     this.$el.html(tpl);
+//     $("#food-container").append(this.$el.html());
+//   }
+// });
+//
+//
+// $(document).ready(function(evt) {
+//
+//   var foodModel = new foodModel();
+//   foodModel.url = '/api/food';
+//
+//   var foodCollection = new foodCollection();
+//   foodCollection.url = '/api/food';
+//   });
+//
+//   foodCollection.fetch();
 
 //
 // // define an app namespace
