9a7d814a11c7e5cb4c88c4c8095bbbcf8bd77172
Dakoda Wogan <d.dakoda@yahoo.com>
2015-04-29 19:34:28 -0500

    session controller update

--- controllers/guests_controller.rb
+++ controllers/guests_controller.rb
@@ -1,38 +1,66 @@
-class GuestsController < Sinatra::Base
+class GuestController < Sinatra::Base
   enable  :sessions
   helpers Sinatra::SessionHelper
-  use Rack::MethodOverride
 
   set :views, File.expand_path('../../views', __FILE__)
   set :public_folder, File.expand_path('../../public', __FILE__)
 
+
+  # ***** Debugging *****
   get '/pry' do
     binding.pry
   end
 
+  # ***** ROUTES *****
   get '/' do
     erb :index
   end
 
   get '/admin' do
-  if current_user
-    erb :authenticated
-  else
-    erb :not_authenticated
-  end
-
-  get '/logout' do
-    session[:current_user] = nil
-    redirect '/'
-  end
-
-  post '/sessions' do
-
-  end
-
-  delete '/sessions' do
-
+    if current_user
+      erb :admin_authenticated
+    else
+      erb :admin_non_authenticated
+    end
   end
 
 end
-end
+
+# class GuestsController < Sinatra::Base
+#   enable  :sessions
+#   helpers Sinatra::SessionHelper
+#   use Rack::MethodOverride
+#
+#   set :views, File.expand_path('../../views', __FILE__)
+#   set :public_folder, File.expand_path('../../public', __FILE__)
+#
+#   get '/pry' do
+#     binding.pry
+#   end
+#
+#   get '/' do
+#     erb :index
+#   end
+#
+#   get '/admin' do
+#   if current_user
+#     erb :authenticated
+#   else
+#     erb :not_authenticated
+#   end
+#
+#   get '/logout' do
+#     session[:current_user] = nil
+#     redirect '/'
+#   end
+#
+#   post '/sessions' do
+#
+#   end
+#
+#   delete '/sessions' do
+#
+#   end
+#
+# end
+# end
--- controllers/session_controller.rb
+++ controllers/session_controller.rb
@@ -1,38 +1,12 @@
-# class SessionController < Sinatra::Base
-#   enable  :sessions
-#   helpers Sinatra::SessionHelper
-#   use Rack::MethodOverride
-#
-#   post '/' do
-#     user = User.find_by(:username => params[:username])
-#     if user && user.password == params[:password]
-#       session[:current_user] = user.id
-#       redirect '/admin'
-#     else
-#       redirect '/'
-#     end
-#   end
-#
-#   delete '/' do
-#     session[:current_user] = nil
-#     redirect '/'
-#   end
-#
-# end
-
-
-class SessionsController < Sinatra::Base
+class SessionController < Sinatra::Base
   enable  :sessions
   helpers Sinatra::SessionHelper
   use Rack::MethodOverride
 
-  # ***** Debugging *****
   get '/pry' do
     binding.pry
   end
 
-
-  # ***** Routes: /sessions *****
   post '/' do
     user = User.find_by(:username => params[:username])
     if user && user.password == params[:password]
@@ -47,5 +21,4 @@ class SessionsController < Sinatra::Base
     session[:current_user] = nil
     redirect '/'
   end
-
 end
