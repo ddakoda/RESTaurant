e95ecf4f7133bdb1a480f8e07ddf0ed3b127086a
Dakoda Wogan <d.dakoda@yahoo.com>
2015-05-04 18:38:23 -0500

    last commit

--- models/food.rb
+++ models/food.rb
@@ -1,41 +1,6 @@
 class Food < ActiveRecord::Base
   has_many(:orders)
-  has_many(:parties, through: :orders)
-
-  # def initialize(name, description, price, nut)
-  #   @name = name
-  #   @description = description
-  #   @price = price
-  #   @nut = true
-  # end
-  #
-  # def name
-  #   @name
-  # end
-  #
-  # def name=(new_name)
-  #   @name = new_name
-  # end
-  #
-  # def description
-  #   @description
-  # end
-  #
-  # def description=(new_description)
-  #   @description = new_description
-  # end
-  #
-  # def price
-  #   @price
-  # end
-  #
-  # def price=(new_price)
-  #   @price = new_price
-  # end
-  #
-  # def nut
-  #   @nut
-  # end
+  has_many(:parties, through: :orders)x
 
   def to_s
     'wake up'
--- models/receipt.rb
+++
@@ -1,6 +0,0 @@
-class Receipt < ActiveRecord::Base
-
-  def to_s
-    'cash-out'
-  end
-end
